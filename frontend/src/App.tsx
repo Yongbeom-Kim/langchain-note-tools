38636b2d36fe91d77cb5e4f754b7da310017dfcc
Kim Yongbeom <dernbu@gmail.com>
2024-09-10 22:57:52 +0800

    feat: Prettier and husky

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { ThemeProvider } from "@/components/theme-provider"
-import { ModeToggle } from './components/mode-toggle'
+import "./App.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <ModeToggle />
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
